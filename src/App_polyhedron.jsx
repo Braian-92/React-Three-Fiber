ac121e38d95ced06c59e4eec2ace8f2296c30c20
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-08 00:20:41 -0300

    base leva leva consola debug

--- src/App_polyhedron.jsx
+++ src/App_polyhedron.jsx
@@ -4,16 +4,23 @@ import * as THREE from 'three'
 import { Stats, OrbitControls } from '@react-three/drei'
 // npm install @react-three/drei --save-dev
 // npm start
+import { useControls } from 'leva'
+// npm install leva --save-dev
 
 export default function App() {
   const polyhedron = [
     new THREE.BoxGeometry(),
     new THREE.SphereGeometry(0.785398),
     new THREE.DodecahedronGeometry(0.785398),
-  ]
+  ];
+
+  const color = useControls({
+    value: 'green',
+  })
 
   return (
     <Canvas camera={{ position: [0, 0, 3] }}>
+      <color attach="background" args={[color.value]} />
       <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
 import App_polyhedron from './App_polyhedron'
+import App_leva from './App_leva'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <App_polyhedron />
+    {/* <App_polyhedron /> */}
+    <App_leva />
   </StrictMode>
 )
