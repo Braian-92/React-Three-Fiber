6c46f2a1ed9adf066b02ef6da8cd97d381febb34
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-07 23:37:48 -0300

    base sharing objects (polyhedron)

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
+import App_polyhedron from './App_polyhedron'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <App_polyhedron />
   </StrictMode>
 )
