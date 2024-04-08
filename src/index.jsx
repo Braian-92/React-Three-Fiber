0de52bb27b15e2085a752feb1eda7b06bbb8a0d4
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-08 00:42:21 -0300

    materiales con leva (base)

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,13 @@ import './styles.css'
 import App from './App'
 import App_polyhedron from './App_polyhedron'
 import App_leva from './App_leva'
+import App_materials from './App_materials'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <App_polyhedron /> */}
-    <App_leva />
+    {/* <App_leva /> */}
+    <App_materials />
   </StrictMode>
 )
