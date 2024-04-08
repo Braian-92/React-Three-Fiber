4638b4beafe8251cff95aff7fb522d9fdc2d98c9
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-08 00:51:09 -0300

    luces con leva (base)

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,14 @@ import App from './App'
 import App_polyhedron from './App_polyhedron'
 import App_leva from './App_leva'
 import App_materials from './App_materials'
+import App_luces from './App_luces'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <App_polyhedron /> */}
     {/* <App_leva /> */}
-    <App_materials />
+    {/* <App_materials /> */}
+    <App_luces />
   </StrictMode>
 )
