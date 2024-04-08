7794e497470f052e5d7cd6dbed3ed4ddf711d286
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-07 23:11:45 -0300

    useState controlar eventos

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,27 +1,33 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default function Box(props){
   const ref = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [rotate, setRotate] = useState(false);
   
   useFrame((state, delta) => {
-    ref.current.rotation.x += 1 * delta;
-    ref.current.rotation.y += 0.5 * delta;
-    ref.current.position.y = Math.sin(state.clock.getElapsedTime()) / 2;
+    if(rotate){
+      ref.current.rotation.x += 1 * delta;
+      ref.current.rotation.y += 0.5 * delta;
+      // ref.current.position.y = Math.sin(state.clock.getElapsedTime()) / 2;
+    }
   });
 
   return (
     <mesh 
       {...props} 
       ref={ref}
-      onPointerDown={(e) => {console.log('pinter Down: ' + e.object.name)}}
-      onPointerUp={(e) => {console.log('pinter Up: ' + e.object.name)}}
-      onPointerOver={(e) => {console.log('pinter Over: ' + e.object.name)}}
-      onPointerOut={(e) => {console.log('pinter Out: ' + e.object.name)}}
+      scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+      onPointerDown={(e) => setRotate(!rotate) }
+      // onPointerUp={(e) => {console.log('pinter Up: ' + e.object.name)}}
+      onPointerOver={(e) => setHover(true) }
+      onPointerOut={(e) => setHover(false) }
       onUpdate={(self) => {console.log(self)}}
     >
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
+      <meshBasicMaterial color={hovered ? 0xff0000 : 0x00ff00} wireframe />
     </mesh>
   );
 }
