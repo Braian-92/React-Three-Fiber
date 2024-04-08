612f2a95b0b938d35621fad0bb170d9a6a99e3d6
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-07 22:12:24 -0300

    useFrame

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,11 +1,14 @@
+import { useFrame } from "@react-three/fiber";
 import { useRef, useEffect } from "react";
 
 export default function Box(props){
   const ref = useRef();
   
-  useEffect(() => {
-    console.log(ref);
-  })
+  useFrame((state, delta) => {
+    ref.current.rotation.x = 1 * delta;
+    ref.current.rotation.y = 0.5 * delta;
+    ref.current.position.y = Math.sin(state.clock.getElapsedTime()) / 2;
+  });
 
   return (
     <mesh {...props} ref={ref}>
