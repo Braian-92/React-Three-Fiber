64096e041ad79baf6fa9a99a7777b248e8d2af5e
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-07 22:20:04 -0300

    eventos y fix rotacion usaFrame

--- src/Box.jsx
+++ src/Box.jsx
@@ -5,13 +5,17 @@ export default function Box(props){
   const ref = useRef();
   
   useFrame((state, delta) => {
-    ref.current.rotation.x = 1 * delta;
-    ref.current.rotation.y = 0.5 * delta;
+    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.y += 0.5 * delta;
     ref.current.position.y = Math.sin(state.clock.getElapsedTime()) / 2;
   });
 
   return (
-    <mesh {...props} ref={ref}>
+    <mesh 
+      {...props} 
+      ref={ref}
+      onPointerDown={() => {console.log(ref.current)}}
+    >
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
