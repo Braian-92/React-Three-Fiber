493e2a7a4b99b2967bd559b56f707124c63b595e
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-13 13:16:44 -0300

    agregar movimiento opcional en direccion de la camara en 2 ejes

--- src/Box_hooks.jsx
+++ src/Box_hooks.jsx
@@ -1,16 +1,50 @@
 import { useRef, useState } from 'react'
-import { useFrame } from '@react-three/fiber'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useControls } from 'leva'
+import * as THREE from 'three'
 
 export default function Box(props) {
   const ref = useRef()
   const keyMap = props.keyMap
   const [selected, setSelected] = useState(props.selected)
+  const { camera } = useThree()
+  
+  // Utilizar Leva para controlar la opción de dirección de la cámara
+  const { direccionCamara } = useControls({
+    direccionCamara: false,
+  })
 
   useFrame((_, delta) => {
-    keyMap['KeyA'] && selected && (ref.current.position.x -= 1 * delta)
-    keyMap['KeyD'] && selected && (ref.current.position.x += 1 * delta)
-    keyMap['KeyW'] && selected && (ref.current.position.z -= 1 * delta)
-    keyMap['KeyS'] && selected && (ref.current.position.z += 1 * delta)
+    if (direccionCamara) {
+      const direction = camera.getWorldDirection(new THREE.Vector3()).setY(0).normalize() // Fijar el componente y a 0
+      const speed = 1
+
+      if (keyMap['KeyA'] && selected) {
+        ref.current.position.addScaledVector(direction.clone().cross(new THREE.Vector3(0, 1, 0)), -speed * delta)
+      }
+      if (keyMap['KeyD'] && selected) {
+        ref.current.position.addScaledVector(direction.clone().cross(new THREE.Vector3(0, 1, 0)), speed * delta)
+      }
+      if (keyMap['KeyW'] && selected) {
+        ref.current.position.add(direction.clone().multiplyScalar(speed * delta)) // Cambiado el signo para el movimiento hacia adelante
+      }
+      if (keyMap['KeyS'] && selected) {
+        ref.current.position.add(direction.clone().multiplyScalar(-speed * delta)) // Cambiado el signo para el movimiento hacia atrás
+      }
+    } else {
+      if (keyMap['KeyA'] && selected) {
+        ref.current.position.x -= 1 * delta
+      }
+      if (keyMap['KeyD'] && selected) {
+        ref.current.position.x += 1 * delta
+      }
+      if (keyMap['KeyW'] && selected) {
+        ref.current.position.z -= 1 * delta
+      }
+      if (keyMap['KeyS'] && selected) {
+        ref.current.position.z += 1 * delta
+      }
+    }
   })
 
   return (
